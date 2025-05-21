f79e015710aacc96ad9a1e0a6e9aba7ea4df161b
rob <robfromdublin@gmail.com>
2025-05-21 22:24:47 +1000

    Attempting to run with service account credentials and github actions

--- parse_url.py
+++ parse_url.py
@@ -4,6 +4,7 @@ import datetime as dt
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
 import os
 import json
 

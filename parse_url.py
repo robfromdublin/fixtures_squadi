667d1e251b76b09f40c05f376699d06eef4f7084
robfromdublin <robfromdublin@gmail.com>
2026-06-23 20:57:42 +1000

    Update parse_url.py
    Added state champs

--- parse_url.py
+++ parse_url.py
@@ -459,6 +459,7 @@ if __name__ == '__main__':
     print('Holmes family fixtures')
     urls = {'Rob': "https://registration.squadi.com/livescoreSeasonFixture?organisationKey=771945e6-27e1-43bf-b81e-30f80d1a4568&yearId=8&competitionUniqueKey=8e0e372e-1695-47e7-a34b-a50cf09f1a36&divisionId=All&teamId=103777",
             'Saoirse': "https://registration.squadi.com/livescoreSeasonFixture?organisationKey=74f39f3a-6e73-48a8-b837-705aba4c4512&yearId=8&competitionUniqueKey=3b8e1b7a-2625-402e-bd40-8da1d816291c&divisionId=All&teamId=92561",
+            'SaoirseState':"https://registration.squadi.com/livescoreSeasonFixture?organisationKey=74f39f3a-6e73-48a8-b837-705aba4c4512&yearId=8&competitionUniqueKey=11f01033-6939-4f79-9438-c97beef960e2&divisionId=All&teamId=123111",
             'Cillian': "a49d026a",
             'Aidan': "aLbZvg5xud8gSgB4q"
            }  
